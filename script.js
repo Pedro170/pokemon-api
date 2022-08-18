b6d3a2ffd73e432b2d62a601d7b97c207057452b
João Pedro <Joaosilva8882@gmail.com>
2022-08-18 12:30:05 -0300

    Adicionado evento de next e prev

--- script.js
+++ script.js
@@ -1,36 +1,52 @@
 const ULR_BASE = "https://pokeapi.co/api/v2/pokemon/";
 
 const foto = document.querySelector("#foto");
-const h1 = document.querySelector('#name-pokemon');
-const order = document.querySelector('.order');
-const btnSelect = document.querySelector('.btn');
-const btnPrev = document.querySelector('.prev');
-const btnNext = document.querySelector('.next');
+const h1 = document.querySelector("#name-pokemon");
+const order = document.querySelector(".order");
+const btnSelect = document.querySelector(".btn");
+const btnPrev = document.querySelector(".prev");
+const btnNext = document.querySelector(".next");
 
-//let pokemon = 1;
+let nextPokemon = 1;
 
-// btnNext.addEventListener('click', () => {
+btnNext.addEventListener("click", () => {
+  nextPokemon += 1;
+  renderPokemon(nextPokemon);
+});
 
-// })
-
-// btnPrev.addEventListener('click', () => {
-
-// })
+const path = document.querySelector("#path");
+btnPrev.addEventListener("click", () => {
+  if( nextPokemon > 1 ) {
+    nextPokemon -= 1;
+    renderPokemon(nextPokemon);
+  } else {
+    path.classList.add("disabled");
+  }
+});
 
 const getPokemon = async () => {
-  const random = 1 + Math.floor(Math.random() * 50 - 1);
-  const response = await fetch(`${ULR_BASE}${random}`);
-  const json = await response.json();
-
-  const img = json.sprites.versions["generation-v"]['black-white'].animated.front_default;
-
-  if( json ) {
-    foto.src = img
-    h1.innerHTML = json.name;
-    order.innerHTML = json.order
-  } else {
-    foto.src = json.sprites.versions["generation-v"]['black-white'].animated.front_default;
+  const response = await fetch(`${ULR_BASE}${nextPokemon}`);
+  if( response.ok ) {
+    const json = await response.json();
+    return json;
   }
 };
 
-getPokemon()
+const renderPokemon = async () => {
+  h1.innerHTML = "carregando..."
+
+  const data = await getPokemon();
+  const img = data.sprites.versions["generation-v"]["black-white"].animated.front_default;
+    if (data) {
+      foto.src = img;
+      h1.innerHTML = data.name;
+      order.innerHTML = data.order;
+    } else {
+      foto.src =
+        data.sprites.versions["generation-v"][
+          "black-white"
+        ].animated.front_default;
+    }
+}
+
+renderPokemon(nextPokemon);
